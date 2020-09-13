b4d4499c1a1165925ad5786e0f34dd0e0cd49b09
DevarshNewar <69204233+DevarshNewar@users.noreply.github.com>
2020-09-13 19:24:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,56 @@
-var bullet,wall;
-var speed,weight;
-var damage,thickness;
-//var tracks
-
-function setup() {
-  createCanvas(1600,400);
-
-  //creating speed and weight for bullet
-  speed = random(223,321);
-  weight = random(30,52);
-
-  thickness = random(22,83);
-
-  //creating bullet and wall
-  bullet = createSprite(50, 200, 50, 50);
-  bullet.shapeColor = color(0,0,0);
-  wall = createSprite(1200, 200, thickness, height/2);
   
-  //giving speed to the bullet
-  bullet.velocityX = speed;
+ var bullet, wall, speed, weight, thickness;
+
+ function setup() {
+  createCanvas(1500,150);
+  
+  speed= random(223,321);
+  weight= random(30,50);
+  thickness= random(22,85);
+
+  bullet=createSprite(50, 80, 10, 50);
+  bullet.shapeColor= "white";
+
+  wall= createSprite(1450,70,20,150);
+  wall.shapeColor= "brown";
+
+  bullet.velocityX= speed;
+
+ }
+
+ function draw() {
+
+  background("lightblue");  
  
-}
+  if (collide(bullet,wall)) {
 
-function draw() {
-  background(0); 
-  //giving colour to the wall
-  wall.shapeColor = color(80,80,80);
+  bullet.velocityX=0;
+  var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+  if(damage>10){
+
+  wall.shapeColor= "red";
   
-  //creating track lines
-  //for (let i = 0; i < 1100; i = i + 200) {
-  //  tracks = createSprite(i,200,100,30);    
-  //}
+  }
+  if(deformation<10){
 
-  //danger stste
-  if (isTouching(wall,bullet)) {
-   bullet.shapeColor = color(250,0,0);
-  }else{  
-   bullet.shapeColor = color(0,250,0);
+  wall.shapeColor= "green";
+  
+  }
+
   }
 
   drawSprites();
-}
+
+ }
+
+ function collide (bullet, wall) {
+   
+  bulletREdge= bullet.x + bullet.width;
+  wallLEdge= wall.x;
+  if(bulletREdge>= wallLEdge) {
+  return true;
+
+ }
+  return false;
+  
+ }
